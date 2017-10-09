d437fbc27bda1a7f228d93329e6a1e94a6da23f6
Andy S <andyast@gmail.com>
2017-10-09 13:10:07 -0500

    changing if not exists to MERGE to 100% prevent duplicates

--- dbo.assignToSeries.StoredProcedure.sql
+++ dbo.assignToSeries.StoredProcedure.sql
@@ -12,31 +12,13 @@ CREATE PROCEDURE [dbo].[assignToSeries]
 	@UnsubscribeToken nchar(30)
 )
 AS
-DECLARE
-	@count int
 
-	SELECT @count = 0
-	
-	SELECT @count = COUNT(*) FROM DiscipleshipSeriesUserConnection WHERE SeriesID = @SeriesID
-	AND UserID = @UserID
-IF (@count = 0)
-BEGIN
-	/* Insert user into series starting now, and last sent is 0 */
-	INSERT INTO DiscipleshipSeriesUserConnection
-	(
-		SeriesID,
-		UserID,
-		LastSection,
-		StartDate,
-		UnsubscribeToken
-	) OUTPUT Inserted.SeriesUserConnectionID
-	VALUES
-	(
-		@SeriesID,
-		@UserID,
-		0,
-		convert(date, getdate()),
-		@UnsubscribeToken
-	)
-END
+MERGE DiscipleshipSeriesUserConnection as [Target]
+USING  (select @UserID as UserId, @SeriesID as SeriesId) as [Source]
+	(UserID, SeriesID) on [Target].UserId =[Source].UserId AND [Target].SeriesID = [Source].SeriesID
+WHEN NOT MATCHED THEN
+    INSERT (SeriesID,UserID,LastSection,StartDate,UnsubscribeToken)
+	VALUES (@SeriesID,@UserID,0,convert(date, getdate()),@UnsubscribeToken);
+
+
 GO
