4e9ee8849f50e57a9aa5fa3decceca67c2ad9af8
Andy S <andyast@gmail.com>
2018-01-22 12:18:03 -0600

    adding drop and output inserted so we know if any

--- dbo.assignToSeries.StoredProcedure.sql
+++ dbo.assignToSeries.StoredProcedure.sql
@@ -1,5 +1,10 @@
 USE [_connector]
 GO
+IF OBJECT_ID('assignToSeries') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[assignToSeries];
+END
+GO
 /****** Object:  StoredProcedure [dbo].[assignToSeries]    Script Date: 8/16/2017 8:45:17 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -18,7 +23,8 @@ USING  (select @UserID as UserId, @SeriesID as SeriesId) as [Source]
 	(UserID, SeriesID) on [Target].UserId =[Source].UserId AND [Target].SeriesID = [Source].SeriesID
 WHEN NOT MATCHED THEN
     INSERT (SeriesID,UserID,LastSection,StartDate,UnsubscribeToken)
-	VALUES (@SeriesID,@UserID,0,convert(date, getdate()),@UnsubscribeToken);
+	VALUES (@SeriesID,@UserID,0,convert(date, getdate()),@UnsubscribeToken)
+OUTPUT inserted.*;  
 
 
 GO
